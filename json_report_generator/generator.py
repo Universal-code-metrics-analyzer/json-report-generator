d67023d270a7366dae8597d3642d703ea8ac9872
Alex Pirogov <pirogovalexandr00@mail.ru>
2024-05-05 17:40:30 +0300

    feat: reports dir

--- json_report_generator/generator.py
+++ json_report_generator/generator.py
@@ -1,15 +1,20 @@
+from pathlib import Path
+
 import aiofiles
 from core.report_generator import ReportGenerator, ReportGenratorConfigShape
-from pydantic import FilePath
 
 
 class JsonReportGenratorConfigShape(ReportGenratorConfigShape):
-    output_path: FilePath
+    output_dir: Path
 
 
 class JsonReportGenerator(
     ReportGenerator[JsonReportGenratorConfigShape], config_shape=JsonReportGenratorConfigShape
 ):
     async def generate(self) -> None:
-        async with aiofiles.open(self.config.output_path, 'w') as file:
+        if not self.config.output_dir.exists():
+            self.config.output_dir.mkdir(parents=True, exist_ok=True)
+
+        file_name = self.commit_sha + '.json'
+        async with aiofiles.open(self.config.output_dir / file_name, 'w') as file:
             await file.write(self.tree_metrics.model_dump_json(indent=4))
