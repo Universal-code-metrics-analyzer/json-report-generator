5c4030996254eb4b304f734af236a28e1f55e081
Alex Pirogov <pirogovalexandr00@mail.ru>
2024-05-05 16:39:12 +0300

    fix: generate method

--- json_report_generator/generator.py
+++ json_report_generator/generator.py
@@ -1,5 +1,4 @@
 import aiofiles
-from core.metrics_calculator import TreeMetrics
 from core.report_generator import ReportGenerator, ReportGenratorConfigShape
 from pydantic import FilePath
 
@@ -11,6 +10,6 @@ class JsonReportGenratorConfigShape(ReportGenratorConfigShape):
 class JsonReportGenerator(
     ReportGenerator[JsonReportGenratorConfigShape], config_shape=JsonReportGenratorConfigShape
 ):
-    async def generate(self, tree_metrics: TreeMetrics) -> None:
+    async def generate(self) -> None:
         async with aiofiles.open(self.config.output_path, 'w') as file:
-            await file.write(tree_metrics.model_dump_json(indent=4))
+            await file.write(self.tree_metrics.model_dump_json(indent=4))
