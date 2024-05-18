5df3ae1bfb1ecc6ba083d664099e0237b3537371
Alex Pirogov <pirogovalexandr00@mail.ru>
2024-05-18 15:30:03 +0300

    fix: fix core incompatibilities

--- json_report_generator/generator.py
+++ json_report_generator/generator.py
@@ -15,6 +15,6 @@ class JsonReportGenerator(
         if not self.config.output_dir.exists():
             self.config.output_dir.mkdir(parents=True, exist_ok=True)
 
-        file_name = self.ref + '.json'
+        file_name = self.sha + '.json'
         async with aiofiles.open(self.config.output_dir / file_name, 'w') as file:
             await file.write(self.tree_metrics.model_dump_json(indent=4))
